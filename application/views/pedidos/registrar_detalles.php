7019dc4444457844755e1d4ddf6c302d510fc986
Jonathan Cabrera <jcabrera.moreno@gmail.com>
2012-10-10 19:25:12 -0500

    Menu changed "Oppa Cristina style"

--- application/views/pedidos/registrar_detalles.php
+++ application/views/pedidos/registrar_detalles.php
@@ -174,7 +174,7 @@
 			<tr>
 				<td></td>
 				<td></td>
-				<td style="text-align:right"><b>IVA (<?php echo $sucursal['iva']*100; ?>%)</b></td>
+				<td style="text-align:right"><b>IVA (<?php echo $sucursal['iva']*100; ?>%):</b></td>
 				<td style="text-align:right">$<?php echo number_format($sucursal['iva']*$subtotal, 2, '.', ','); ?></td>
 				<td></td>
 			</tr>
