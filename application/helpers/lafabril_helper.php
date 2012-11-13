390004f0c9a8b9755947be7ace5daf51120dbdd7
Gastón Omar Sánchez Ruíz <gaston.o.sanchez@gmail.com>
2012-11-12 19:04:57 -0600

    Fixes create bill

--- application/helpers/lafabril_helper.php
+++ application/helpers/lafabril_helper.php
@@ -54,3 +54,21 @@ if (!function_exists('convertToHumanDate')) {
 		return false;
 	}
 }
+
+/*
+ * Converts date from dd/mm/yyyy format to yyyy-mm-dd
+ * In case of receive an invalid date, returns false
+ */
+if (!function_exists('convertToComputerDate')) {
+	function convertToComputerDate($date)
+	{
+		$oldDate = $date;
+		$date = explode('/', $date);
+		
+		if (count($date) === 3 && checkdate($date[1], $date[0], $date[2])) {
+			return $date[2] . '-' . $date[1] . '-' . $date[0];
+		}
+		
+		return false;
+	}
+}
