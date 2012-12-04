e7e56171c4b24077d434f531239f873bee8ecb9e
Jonathan Cabrera <jcabrera.moreno@gmail.com>
2012-12-03 23:07:33 -0600

    The wallet report finally ready

--- application/views/reportes_financieros/movimientos.php
+++ application/views/reportes_financieros/movimientos.php
@@ -14,58 +14,103 @@
 </table>
 
 <?php if (count($wallet) > 0) : ?>
+
+<?php $gran_total = 0.0; ?>
+
+<?php foreach ($wallet as $client) : ?>
+
+<?php $total_cliente = 0.0 ?>
+
+<h3><?php echo $client['name'] ?></h3>
+
 <table class="catalog">
 	<thead>
 		<tr>
-		   <th></th>
-		   <th>Folio Factura</th>
-		   <th>Folio Pago/NC</th>
+		   <th>Factura</th>
+		   <th>Pago / Nota de Crédito</th>
 		   <th class="textAlign-right">Fecha</th>
-		   <th class="textAlign-right">Fecha Vencimiento</th>
-		   <th class="textAlign-right">Importe Orig Factura</th>
-		   <th class="textAlign-right">Importes</th>
+		   <th class="textAlign-right">Vencimiento</th>
+		   <th class="textAlign-right">Importe Original</th>
+		   <th class="textAlign-right">Importe</th>
 		</tr>
 	</thead>
 
 	<tbody>
-	<?php foreach ($wallet as $client_id => $client) : ?>
-		<tr>
-			<td><?php echo $client['name'] ?></td>
-		</tr>
+		<?php foreach ($client['invoices'] as $invoice) : ?>
 		
-		<?php foreach ($client[$client_id]['invoices'] as $invoice) : ?>
+			<?php $total = $invoice['importe'];?>
+			
+				<tr>
+				   <th><?php echo getFolio($invoice['prefijo'], $invoice['folio']); ?></th>
+				   <th></th>
+				   <th class="textAlign-right"><?php echo convertToHumanDate($invoice['fecha']); ?></th>
+				   <th class="textAlign-right"><?php echo convertToHumanDate($invoice['fecha_vencimiento']); ?></th>
+				   <th class="textAlign-right">$<?php echo getMoneyFormat($invoice['importe']); ?></th>
+				   <th class="textAlign-right"></th>
+				</tr>
+				
+				<?php foreach ($invoice['payments'] as $payment) : ?>
+					<tr>
+					   <th></th>
+					   <th><?php echo getFolio($payment['prefijo'], $payment['folio']); ?></th>
+					   <th class="textAlign-right"><?php echo convertToHumanDate($payment['fecha']); ?></th>
+					   <th class="textAlign-right"></th>
+					   <th class="textAlign-right"></th>
+					   <th class="textAlign-right">$<?php echo getMoneyFormat($payment['importe']); ?></th>
+					</tr>
+					<?php $total -= $payment['importe']; ?>
+				<?php endforeach; ?>
+				
+				<?php foreach ($invoice['credit_notes'] as $credit_note) : ?>
+					<tr>
+					   <th></th>
+					   <th><?php echo getFolio($credit_note['prefijo'], $credit_note['folio']); ?></th>
+					   <th class="textAlign-right"><?php echo convertToHumanDate($credit_note['fecha']); ?></th>
+					   <th class="textAlign-right"></th>
+					   <th class="textAlign-right"></th>
+					   <th class="textAlign-right">$<?php echo getMoneyFormat($credit_note['importe']); ?></th>
+					</tr>
+					<?php $total -= $credit_note['importe']; ?>
+				<?php endforeach; ?>
+				
+				<tr>
+				   <th></th>
+				   <th></th>
+				   <th></th>
+				   <th></th>
+				   <th></th>
+				   <th class="textAlign-right" style="border-top: 1px solid black;">$<?php echo getMoneyFormat($total); ?></th>
+				</tr>
+				
+				<tr>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+				</tr>
+				
+				<?php $total_cliente += $total; ?>
+			
+			<?php endforeach; ?>
+		
 			<tr>
-			   <th></th>
-			   <th><?php echo getFolio(invoice['prefijo'], $invoice['folio']); ?></th>
-			   <th></th>
-			   <th class="textAlign-right"><?php echo convertToHumanDate($invoice['fecha']); ?></th>
-			   <th class="textAlign-right"><?php echo convertToHumanDate($invoice['fecha_vencimiento']); ?></th>
-			   <th class="textAlign-right">$<?php echo getMoneyFormat($invoice['importe']); ?></th>
-			   <th class="textAlign-right"></th>
+			   <th class="textAlign-right" colspan="5">Total <?php echo $client['name'] ?>:</th>
+			   <th class="textAlign-right">$<?php echo getMoneyFormat($total_cliente); ?></th>
 			</tr>
 			
-			<?php foreach ($invoice)
-		<?php endforeach; ?>
-	<?php endforeach; ?>
-	</tbody>
+			<?php $gran_total += $total_cliente; ?>
 	
+	</tbody>
+</table>
+
+<?php endforeach; ?>
+	
+<table class="catalog" style="margin-top:20px">
 	<tfoot>
-		<?php foreach ($payments as $payment) : ?>
 		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<th class="textAlign-center"><?php echo $payment['nombre']; ?></th>
-			<td class="textAlign-right">$<?php echo getMoneyFormat($payment['total']); ?></td>
-		</tr>
-		<?php endforeach; ?>
-		
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<th class="textAlign-center">Total</th>
-			<td class="textAlign-right">$<?php echo getMoneyFormat($total); ?></td>
+			<th class="textAlign-right" style="font-size:110%">Gran Total: $<?php echo getMoneyFormat($gran_total); ?></th>
 		</tr>
 	</tfoot>
 </table>
