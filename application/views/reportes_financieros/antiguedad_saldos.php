de212e2785c5175df87d4c11509d4045b5c72493
Gastón Omar Sánchez Ruíz <gaston.o.sanchez@gmail.com>
2012-12-04 13:31:17 -0600

    Advances on antiquity report

---
+++ application/views/reportes_financieros/antiguedad_saldos.php
@@ -0,0 +1,60 @@
+<table class="filters">
+	<tr>
+		<th>Sucursal</th>
+		<td><?php echo $branch; ?></td>
+		<th>De cliente</th>
+		<td><?php echo $from_client; ?></td>
+	</tr>
+
+	<tr>
+		<th>Fecha de corte</th>
+		<td><?php echo $cutDate; ?></td>
+		<th>A cliente</th>
+		<td><?php echo $to_client; ?></td>
+</table>
+
+<?php if (count($clients) > 0) : ?>
+
+<?php foreach ($clients as $client) : ?>
+
+<h3><?php echo $client['nombre_cliente'] ?></h3>
+
+<table class="catalog">
+	<thead>
+		<tr>
+		   <th>Factura</th>
+		   <th class="textAlign-right">Fecha</th>
+		   <th class="textAlign-right">Vencimiento</th>
+		   <th class="textAlign-right">Saldo</th>
+		   <th class="textAlign-right">Vencido</th>
+		   <th class="textAlign-right">1 - 30</th>
+		   <th class="textAlign-right">31 - 60</th>
+		   <th class="textAlign-right">61 - 90</th>
+		   <th class="textAlign-right">91 - 120</th>
+		   <th class="textAlign-right">+120</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php foreach ($client['invoices'] as $invoice) : ?>
+			
+				<tr>
+				   <td><?php echo getFolio($invoice['prefijo'], $invoice['folio']); ?></td>
+				   <td class="textAlign-right"><?php echo convertToHumanDate($invoice['fecha']); ?></td>
+				   <td class="textAlign-right"><?php echo convertToHumanDate($invoice['fecha_vencimiento']); ?></td>
+				   <td class="textAlign-right">$<?php echo getMoneyFormat($invoice['saldo']); ?></td>
+				   <td class="textAlign-right"><?php if ($invoice['daysToFallDue'] === 0) { echo '$'.getMoneyFormat($invoice['saldo']); } ?></td>
+				   <td class="textAlign-right"><?php if ($invoice['daysToFallDue'] >= 1 && $invoice['daysToFallDue'] <= 30) { echo '$'.getMoneyFormat($invoice['saldo']); } ?></td>
+				   <td class="textAlign-right"><?php if ($invoice['daysToFallDue'] >= 31 && $invoice['daysToFallDue'] <= 60) { echo '$'.getMoneyFormat($invoice['saldo']); } ?></td>
+				   <td class="textAlign-right"><?php if ($invoice['daysToFallDue'] >= 61 && $invoice['daysToFallDue'] <= 90) { echo '$'.getMoneyFormat($invoice['saldo']); } ?></td>
+				   <td class="textAlign-right"><?php if ($invoice['daysToFallDue'] >= 91 && $invoice['daysToFallDue'] <= 120) { echo '$'.getMoneyFormat($invoice['saldo']); } ?></td>
+				   <td class="textAlign-right"><?php if ($invoice['daysToFallDue'] > 120 ) { echo $invoice['saldo']; } ?></td>
+				</tr>
+			
+			<?php endforeach; ?>
+	</tbody>
+</table>
+
+<?php endforeach; ?>
+
+<?php endif; ?>
